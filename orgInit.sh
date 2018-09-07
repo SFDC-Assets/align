527320e2876dd8e761cfea7ef9e9374dece08882
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-07 12:13:47 -0500

    automate heroku deploy (works locally!)

--- orgInit.sh
+++ orgInit.sh
@@ -4,3 +4,6 @@ sfdx force:user:permset:assign -n Align_Concierge
 sfdx force:apex:execute -f scripts/makeData.cls
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/o/Lead/list?filterName=All_Leads
+
+sfdx shane:heroku:repo:deploy -g mshanemc -r align-web-demo -n `basename "${PWD}"` -t autodeployed-demos -o DOCTOR_ID=`sfdx shane:data:id:query -o Account -w "Name = 'Bella Smile'"`
+heroku access:update platformpmmdemos@gmail.com -a `basename "${PWD}"` --permissions=deploy,operateherok
