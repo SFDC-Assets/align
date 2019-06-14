3b0e5c8cf5b51a8a3a3691e556d10c9baa35486c
mshanemc <shane.mclaughlin@salesforce.com>
2019-06-14 12:01:01 -0500

    add heroku connect command

--- orgInit.sh
+++ orgInit.sh
@@ -7,3 +7,4 @@ sfdx force:org:open -p /lightning/o/Lead/list?filterName=All_Leads
 
 sfdx shane:heroku:repo:deploy -g mshanemc -r align-web-demo -n `basename "${PWD}"` -t autodeployed-demos -o DOCTOR_ID=`sfdx shane:data:id:query -o Account -w "Name = 'Bella Smile'"`
 heroku access:add platformpmmdemos@gmail.com -a `basename "${PWD}"` --permissions deploy,operate,manage
+sfdx shane:heroku:connect -a `basename "${PWD}"` -f config/invisalign-web-demo.json -e custom
